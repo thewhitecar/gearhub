8f7229b828ccbaee19a15a6b3aac3b524e446229
Ryan Whitecar <ryanwhitecar@gmail.com>
2018-08-31 09:03:16 -0600

    form p1 complete

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,75 +1,172 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import {update_make, update_model, update_serial, update_condition, update_category} from '../../redux/reducers/form_reducer'
-import { Link } from 'react-router-dom';
-import { logOut } from '../../redux/reducers/auth_reducer'
-import categoryArray from '../../jsData/categoryVariables';
-import conditionArray from '../../jsData/conditionArray';
-import './form.css';
-import rightArrow from './right.png';
-import menu_icon from './menu.png';
-import add_icon from './add.png';
-import logo from './gearHubWhite.png'
-import logout_icon from './logout.png'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import {
+    update_make,
+    update_model,
+    update_serial,
+    update_condition,
+    update_category
+} from "../../redux/reducers/form_reducer";
+import { Link } from "react-router-dom";
+import { logOut } from "../../redux/reducers/auth_reducer";
+import categoryArray from "../../jsData/categoryVariables";
+import conditionArray from "../../jsData/conditionArray";
+import SideDrawer from "../sidedrawer/sideDrawer";
+import Backdrop from "../backdrop/backdrop";
+import menu_icon from "../buttons/menu.png";
+import add_icon from "../buttons/add.png";
+import logo from "../buttons/logo.png";
+import logout_icon from "../buttons/logout.png";
+import rightArrow from "../buttons/right.png";
+import "./form.css";
 
+class Form extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            sideDrawerOpen: false
+        };
+    }
 
-class Form extends Component{
+    drawerToggleClickHandler = () => {
+        this.setState(prevState => {
+            return { sideDrawerOpen: !prevState.sideDrawerOpen };
+        });
+    };
+
+    backdropClickHandler = () => {
+        this.setState({ sideDrawerOpen: false });
+    };
 
     render() {
-        const {update_make, update_model, update_serial, update_condition, update_category} = this.props
-        let categoryMap= [];
+        let backdrop;
+        if (this.state.sideDrawerOpen) {
+            backdrop = <Backdrop click={this.backdropClickHandler} />;
+        }
+
+        const {
+            update_make,
+            update_model,
+            update_serial,
+            update_condition,
+            update_category
+        } = this.props;
+        let categoryMap = [];
         categoryMap = categoryArray.map(e => {
-            return(
-                <option value={`${e.categoryPath}`}>{`${e.categoryPath}`}</option>
-        )})
-        let conditionsMap= [];
+            return <option value={`${e.categoryPath}`}>{`${e.categoryPath}`}</option>;
+        });
+        let conditionsMap = [];
         conditionsMap = conditionArray.map(e => {
-            return(
-                <option value={`${e.display_name}`}>{`${e.display_name}`}</option>
-            )})
+            return <option value={`${e.display_name}`}>{`${e.display_name}`}</option>;
+        });
         return (
             <div className="background">
+                <SideDrawer show={this.state.sideDrawerOpen} />
+                {backdrop}
                 <div className="header">
-                    <img src={menu_icon} className="buttons" alt="" />
-                    <Link to="/form"><img alt="" src={add_icon} className="buttons" /></Link>
-                    <Link to="/"><img src={logout_icon} alt="logout" onClick={this.props.logOut} className="buttons"/></Link>
-                    <div className="spacer" />
-                    <Link to="/dash"><img  alt="" src={logo} className="logo"/></Link>
+                    <div className="spacer-left" />
+                    <img
+                        onClick={this.drawerToggleClickHandler}
+                        src={menu_icon}
+                        className="menu-button"
+                        alt="Menu"
+                    />
+                    <Link to="/form">
+                        <img alt="Add Gear" src={add_icon} className="buttons" />
+                    </Link>
+                    <Link to="/">
+                        <img
+                            src={logout_icon}
+                            alt="logout"
+                            onClick={this.props.logOut}
+                            className="buttons"
+                        />
+                    </Link>
+                    <div className="spacer-mid" />
+                    <Link to="/dash">
+                        <img alt="" src={logo} className="logo" />
+                    </Link>
                 </div>
-                <div className="content-box">
-                <div className="spacer-left-arrow"/>
-            <div className="form-box">
-            <h1 className="heading"> add gear to your inventory </h1>
-            <input className="input-field" value={this.props.make} placeholder="make" onChange= {( e ) => update_make( e.target.value )}></input>
-            <input className="input-field" value={this.props.model}placeholder="model" onChange= {( e ) => update_model( e.target.value )}></input>
-            <input className="input-field" value={this.props.serial} placeholder="serial" onChange= {( e ) => update_serial( e.target.value )}></input>
-            <select className="input-field" value={this.props.condition} onChange = { ( e ) => update_condition( e.target.value ) }>
-                <option className ="input-field"  value="">condition</option>
-                {conditionsMap}
-            </select>
-            <select className="input-field" value={this.props.category} onChange = {(e) => update_category(e.target.value)}>
-                <option className ="input-field" value="">category</option>
-                {categoryMap}
-            </select>
-            </div>
-            <Link to="/form2"><img src={rightArrow} alt="next" className="right_arrow"/></Link>
-            </div>
+                <div className="dashboard-content-box">
+                    <div className="titleDiv">
+                        <span className="whiteSpan">Add Gear </span>
+                        <span className="orangeSpan">To Your Inventory</span>
+                    </div>
+                    <div className="form-wrapper">
+                        <div className="form-left-spacer"></div>
+                        <div className="form-content">
+                            <input
+                                className="input-field"
+                                value={this.props.make}
+                                placeholder="make"
+                                onChange={e => update_make(e.target.value)}
+                            />
+                            <input
+                                className="input-field"
+                                value={this.props.model}
+                                placeholder="model"
+                                onChange={e => update_model(e.target.value)}
+                            />
+                            <input
+                                className="input-field"
+                                value={this.props.serial}
+                                placeholder="serial"
+                                onChange={e => update_serial(e.target.value)}
+                            />
+                            <select
+                                className="input-field"
+                                value={this.props.condition}
+                                onChange={e => update_condition(e.target.value)}
+                            >
+                                <option disabled hidden className="input-field" value="">
+                                    condition
+            </option>
+                                {conditionsMap}
+                            </select>
+                            <select
+                                className="input-field"
+                                value={this.props.category}
+                                onChange={e => update_category(e.target.value)}
+                            >
+                                <option disabled hidden className="input-field" value="">
+                                    category
+            </option>
+                                {categoryMap}
+                            </select>
+                        </div>
+                    
+                    <div className="form-right-spacer">
+                    <Link to="/form2">
+                        <img src={rightArrow} alt="next" className="right_arrow" />
+                    </Link>
+                    </div>
+                    </div>
                 </div>
-            )
+            </div>
+        );
     }
 }
 
+function mapStateToProps(state) {
+    const { make, model, serial, condition, category } = state.form;
 
-function mapStateToProps( state ) {
-    const { make, model, serial, condition, category} = state.form;
-  
     return {
-      make,
-      model,
-      serial,
-      condition,
-      category,
+        make,
+        model,
+        serial,
+        condition,
+        category
     };
-  }
-  
-export default connect(mapStateToProps,{update_make, update_model, update_serial, update_condition, update_category, logOut})(Form);
+}
+
+export default connect(
+    mapStateToProps,
+    {
+        update_make,
+        update_model,
+        update_serial,
+        update_condition,
+        update_category,
+        logOut
+    }
+)(Form);
--- src/components/form2/form2.js
+++ src/components/form2/form2.js
@@ -1,98 +1,165 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
-import { logOut } from '../../redux/reducers/auth_reducer'
-import menu_icon from './menu.png';
-import add_icon from './add.png';
-import logo from './gearHubWhite.png'
-import left from './left.png'
-import right from './right.png'
-import './form2.css';
-import ReactS3Uploader from 'react-s3-uploader';
-import { update_photo1, update_photo2, update_photo3, update_photo4 } from '../../redux/reducers/photos_reducer'
-import logout_icon from './logout.png'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { logOut } from "../../redux/reducers/auth_reducer";
+import Backdrop from "../backdrop/backdrop";
+import SideDrawer from "../sidedrawer/sideDrawer";
+import menu_icon from "../buttons/menu.png";
+import add_icon from "../buttons/add.png";
+import logo from "../buttons/logo.png";
+import logout_icon from "../buttons/logout.png";
+import right from "../buttons/right.png";
+import left from "../buttons/left.png";
 
+import "./form2.css";
+import ReactS3Uploader from "react-s3-uploader";
+import {
+  update_photo1,
+  update_photo2,
+  update_photo3,
+  update_photo4
+} from "../../redux/reducers/photos_reducer";
 
 class Form2 extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            uploaderVisible: true
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      uploaderVisible: true,
+      sideDrawerOpen: false
+    };
+  }
+
+  toggleUploader() {
+    this.setState({
+      uploaderVisible: false
+    });
+  }
+
+  drawerToggleClickHandler = () => {
+    this.setState(prevState => {
+      return { sideDrawerOpen: !prevState.sideDrawerOpen };
+    });
+  };
+
+  backdropClickHandler = () => {
+    this.setState({ sideDrawerOpen: false });
+  };
+
+  onFinish = response => {
+    const {
+      update_photo1,
+      update_photo2,
+      update_photo3,
+      update_photo4
+    } = this.props;
+    let imageUrl = response.signedUrl.split("?").shift();
+    if (this.props.photo1 === "/static/media/none.db88a501.png") {
+      update_photo1(imageUrl);
+    } else if (this.props.photo2 === "/static/media/none.db88a501.png") {
+      update_photo2(imageUrl);
+    } else if (this.props.photo3 === "/static/media/none.db88a501.png") {
+      update_photo3(imageUrl);
+    } else if (this.props.photo4 === "/static/media/none.db88a501.png") {
+      update_photo4(imageUrl);
+      this.toggleUploader();
+    } else {
+      alert("You can only upload four photos for each instrument");
+    }
+  };
+
+  render() {
+    let backdrop;
+    if (this.state.sideDrawerOpen) {
+      backdrop = <Backdrop click={this.backdropClickHandler} />;
     }
 
-    toggleUploader() {
-        this.setState({
-            uploaderVisible: false
-        })
-    }
+    let Uploader = (
+      <ReactS3Uploader
+        className="uploader"
+        signingUrl="/s3/sign"
+        accept="image/*"
+        onFinish={this.onFinish}
+        server={process.env.REACT_APP_SERVERURL}
+        uploadRequestHeaders={{ "x-amz-acl": "public-read" }}
+      />
+    );
 
-    onFinish = (response) => {
-        const { update_photo1, update_photo2, update_photo3, update_photo4 } = this.props;
-        let imageUrl = response.signedUrl.split('?').shift();
-        if (this.props.photo1 === "/static/media/none.db88a501.png") {
-            update_photo1(imageUrl);
-        } else if (this.props.photo2 === "/static/media/none.db88a501.png") {
-            update_photo2(imageUrl);
-        } else if (this.props.photo3 === "/static/media/none.db88a501.png") {
-            update_photo3(imageUrl)
-        } else if (this.props.photo4 === "/static/media/none.db88a501.png") {
-            update_photo4(imageUrl);
-            this.toggleUploader();
-        } else { alert("You can only upload four photos for each instrument") }
-
-    }
-
-    render() {
-
-        let Uploader = <ReactS3Uploader
-            signingUrl="/s3/sign"
-            accept="image/*"
-            onFinish={this.onFinish}
-            server={process.env.REACT_APP_SERVERURL}
-            uploadRequestHeaders={{ 'x-amz-acl': 'public-read' }} />
-
-        return (
-            <div className="background">
-                <div className="header">
-                    <img src={menu_icon} className="buttons" alt="" />
-                    <img alt="" src={add_icon} onClick={this.toggleAddGear} className="buttons" />
-                    <Link to="/"><img src={logout_icon} alt="logout" onClick={this.props.logOut} className="buttons" /></Link>
-                    <div className="spacer" />
-                    <Link to="/dash"><img alt="" src={logo} className="logo" /></Link>
-                </div>
-                <div className="content-box">
-                    <div className="left-div-photos">
-                        <Link to="/form"><img src={left} alt="" /></Link>
-                    </div>
-                    <div className="photos-center-div">
-                        <h1 className="photos-title"> add photos </h1>
-                        {this.state.uploaderVisible && Uploader}
-                        <div className="photos_box">
-                            <img alt="" className="photo" src={this.props.photo1} />
-                            <img alt="" className="photo" src={this.props.photo2} />
-                            <img alt="" className="photo" src={this.props.photo3} />
-                            <img alt="" className="photo" src={this.props.photo4} />
-                        </div>
-                    </div>
-                    <div className="right-div">
-                        <Link to="/form3"><img src={right} alt="" /></Link>
-                    </div>
-                </div>
+    return (
+      <div className="background">
+        <SideDrawer show={this.state.sideDrawerOpen} />
+        {backdrop}
+        <div className="header">
+          <div className="spacer-left" />
+          <img
+            onClick={this.drawerToggleClickHandler}
+            src={menu_icon}
+            className="menu-button"
+            alt="Menu"
+          />
+          <Link to="/form">
+            <img alt="Add Gear" src={add_icon} className="buttons" />
+          </Link>
+          <Link to="/">
+            <img
+              src={logout_icon}
+              alt="logout"
+              onClick={this.props.logOut}
+              className="buttons"
+            />
+          </Link>
+          <div className="spacer-mid" />
+          <Link to="/dash">
+            <img alt="" src={logo} className="logo" />
+          </Link>
+        </div>
+        <div className="dashboard-content-box">
+          <div className="titleDiv">
+            <span className="whiteSpan">Add </span>
+            <span className="orangeSpan">Photos</span>
+          </div>
+          <div className="form-wrapper">
+            <div className="form-right-spacer">
+              <Link to="/form">
+                <img className="right_arrow" src={left} alt="" />
+              </Link>
             </div>
-        )
-    }
+          <div className="form-content">
+            {this.state.uploaderVisible && Uploader}
+            <div className="uploader-photos-div">
+            <div className="uploader-photos-div-top">
+            <img alt="" className="photo" src={this.props.photo1} />
+            <img alt="" className="photo" src={this.props.photo2} />
+            </div>
+            <div style={{paddingTop: "5vh"}}></div>
+            <div className="uploader-photos-div-bottom">
+            <img alt="" className="photo" src={this.props.photo3} />
+            <img alt="" className="photo" src={this.props.photo4} />
+            </div>
+            </div>
+          </div>
+          <div className="form-right-spacer">
+            <Link to="/form3">
+              <img src={right} className="right_arrow" alt="" />
+            </Link>
+          </div>
+        </div>
+      </div>
+      </div>
+    );
+  }
 }
 
 function mapStateToProps(state) {
-
-    return {
-        user: state.auth.data,
-        photo1: state.photo.photos1,
-        photo2: state.photo.photos2,
-        photo3: state.photo.photos3,
-        photo4: state.photo.photos4
-    };
+  return {
+    user: state.auth.data,
+    photo1: state.photo.photos1,
+    photo2: state.photo.photos2,
+    photo3: state.photo.photos3,
+    photo4: state.photo.photos4
+  };
 }
 
-export default connect(mapStateToProps, { update_photo1, update_photo2, update_photo3, update_photo4, logOut })(Form2);
+export default connect(
+  mapStateToProps,
+  { update_photo1, update_photo2, update_photo3, update_photo4, logOut }
+)(Form2);
